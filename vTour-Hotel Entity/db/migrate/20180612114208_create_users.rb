5617ce7f8513b93a8d4a2535a2c503f6638ff579
BazingaPunk <tsshiek@virtusa.com>
2018-06-12 17:28:08 +0530

    Added User Entity Endpoints

--- vTour-Hotel Entity/db/migrate/20180612114208_create_users.rb
+++ vTour-Hotel Entity/db/migrate/20180612114208_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :password
       t.string :first_name
       t.string :last_name
+      t.string :phone
       t.string :role
 
       t.timestamps
--- vTour-Hotel Entity/db/schema.rb
+++ vTour-Hotel Entity/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_112237) do
+ActiveRecord::Schema.define(version: 2018_06_12_114208) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_06_12_112237) do
     t.string "password"
     t.string "first_name"
     t.string "last_name"
+    t.string "phone"
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
